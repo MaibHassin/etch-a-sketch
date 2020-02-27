000d5ce3dfc6123d46dc596dfc481f33aa5b469d
Maib Hassin Syed <maibhassin@gmail.com>
2020-02-27 10:08:22 +0000

    add knobs animation when mouse move over display area

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 let display = document.querySelector('.display');
+let gridSwitch = document.querySelector('.switch__switch');
+let gridSizeValue = document.querySelector('#resolution');
+
+Grid(gridSizeValue.value);
 
 function generateGrid() {
-    let gridSizeValue = document.querySelector('#resolution');
     if (gridSizeValue.value > 100 || gridSizeValue.value < 5) {
         alert('please enter a number between 5 and 100');
         return;
     }
     display.innerHTML = "";
-
+    gridSwitch.style.transform = '';
 
 
     Grid(gridSizeValue.value);
@@ -38,20 +41,98 @@ function Grid(size) {
         gridSquare.classList.add('gridSquare');
         gridSquare.style.height = `${squareSize}px`;
         gridSquare.style.width = `${squareSize}px`;
-        gridSquare.style.backgroundColor = 'red';
-        gridSquare.style.border = '0.5px solid #fff';
+        gridSquare.style.backgroundColor = '#fff';
+        gridSquare.style.border = '0.5px solid #d9d9d9';
         gridSquare.style.zIndex = 2;
 
         gridContainer.appendChild(gridSquare);
     }
 
+    // knobs animation
+    let rightKnob = document.querySelector('.knob__vertical');
+    let leftKnob = document.querySelector('.knob__horizontal');
+    document.querySelector('.grid').addEventListener('mousemove', e => {
+        if(e.movementY > 0) {
+            switch(e.movementY) {
+                case 1:
+                    rightKnob.style.transform = 'rotate(15deg)';
+                case 2:
+                    rightKnob.style.transform = 'rotate(30deg)';
+                case 3:
+                    rightKnob.style.transform = 'rotate(45deg)';
+                case 4:
+                    rightKnob.style.transform = 'rotate(60deg)';
+                case 5:
+                    rightKnob.style.transform = 'rotate(75deg)';
+                case 6:
+                    rightKnob.style.transform = 'rotate(90deg)';
+                default:
+                    rightKnob.style.transform = 'rotate(120deg)';
+            }
+        } 
+        else if(e.movementY < 0) {
+            switch(e.movementY) {
+                case 1:
+                    rightKnob.style.transform = 'rotate(-15deg)';
+                case 2:
+                    rightKnob.style.transform = 'rotate(-30deg)';
+                case 3:
+                    rightKnob.style.transform = 'rotate(-45deg)';
+                case 4:
+                    rightKnob.style.transform = 'rotate(-60deg)';
+                case 5:
+                    rightKnob.style.transform = 'rotate(-75deg)';
+                case 6:
+                    rightKnob.style.transform = 'rotate(-90deg)';
+                default:
+                    rightKnob.style.transform = 'rotate(-120deg)';
+            }
+        }
+        
+        if(e.movementX > 0) {
+            switch(e.movementX) {
+                case 1:
+                    leftKnob.style.transform = 'rotate(15deg)';
+                case 2:
+                    leftKnob.style.transform = 'rotate(30deg)';
+                case 3:
+                    leftKnob.style.transform = 'rotate(45deg)';
+                case 4:
+                    leftKnob.style.transform = 'rotate(60deg)';
+                case 5:
+                    leftKnob.style.transform = 'rotate(75deg)';
+                case 6:
+                    leftKnob.style.transform = 'rotate(90deg)';
+                default:
+                    leftKnob.style.transform = 'rotate(120deg)';
+            }
+        } 
+        else if(e.movementX < 0) {
+            switch(e.movementX) {
+                case 1:
+                    leftKnob.style.transform = 'rotate(-15deg)';
+                case 2:
+                    leftKnob.style.transform = 'rotate(-30deg)';
+                case 3:
+                    leftKnob.style.transform = 'rotate(-45deg)';
+                case 4:
+                    leftKnob.style.transform = 'rotate(-60deg)';
+                case 5:
+                    leftKnob.style.transform = 'rotate(-75deg)';
+                case 6:
+                    leftKnob.style.transform = 'rotate(-90deg)';
+                default:
+                    leftKnob.style.transform = 'rotate(-120deg)';
+            }
+        }
+    });
+
 }
 
-let gridSwitch = document.querySelector('.switch__switch')
-gridSwitch.onchange = () => alert('ok');
+// grid on/off functionality
 
 gridSwitch.onclick = () => {
-    if (gridSwitch.style.transform == "" || gridSwitch.style.transform == 'translateX(0px)') {
+    if (gridSwitch.style.transform == '') {
         gridSwitch.style.transform = 'translateX(18px)';
 
         let gridSquares = document.querySelectorAll('.gridSquare');
@@ -59,10 +140,10 @@ gridSwitch.onclick = () => {
             square.style.border = 'none';
         });
     } else {
-        gridSwitch.style.transform = 'translateX(0px)';
+        gridSwitch.style.transform = '';
         let gridSquares = document.querySelectorAll('.gridSquare');
         gridSquares.forEach(square => {
-            square.style.border = '0.5px solid #fff';
+            square.style.border = '0.5px solid #d9d9d9';
         });
     }
 }
